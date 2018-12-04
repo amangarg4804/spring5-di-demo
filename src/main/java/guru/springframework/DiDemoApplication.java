991a7d97d1e78764e62075c49a4ff5aecd827132
springframeworkguru <john@springframework.guru>
2018-12-05 01:36:06 +0530

    Upgraded to Spring Boot 2.1.0.RELEASE

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -14,9 +14,9 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("myController");
+		MyController myController = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
+		System.out.println(myController.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
